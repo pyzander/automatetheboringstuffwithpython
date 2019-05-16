a6c9336a9fd5b9f550265257b97bffc96093fd51
pyzander <43153080+pyzander@users.noreply.github.com>
2019-05-16 05:27:04 -0500

    Update Collatz Sequence Chapter 3- Functions Project.py

--- Collatz Sequence Chapter 3- Functions Project.py
+++ Collatz Sequence Chapter 3- Functions Project.py
@@ -3,25 +3,32 @@
 
 
 print('Please enter an integer.')
-numberinput=input()
 
 def collatz(number):
     try:
         number=abs(int(number)) #all neg integers will be converted to a positive integer 
-        while True: #not doing 'while number >= 1:' or 'while number != 1:' to account for all number possibilities
-            if number == 0:
-                print(1)
+        while True: #not doing 'while number >= 1:' or 'while number != 1:' to account for all number possibilitie
+            if number == 1:
+                return 1
                 break
-            elif number == 1:
+            elif number == 0:
+                #print(1)
+                #break
+                print('Sorry, 0 gives you a continuous loop. Try again.')
+                numberinputtoreplacezero=input()
+                collatz(numberinputtoreplacezero)
                 break
             elif number % 2 == 0: #and (number != 0 or 1): # % gives us remainder #even
-                number=number//2 #number is divided by 2, rounded to the next smallest whole number #technically i can just do number/2 since i converted number into an integer
+                number=number//2 #// get us answer when a is divided by b, rounded to the next smallest whole number
+                #technically i can just do number/2 since i converted number into an integer
             elif number%2==1: #odd
                 number=3*number+1
-            print(number)          
+            print(number)
+        
     except:
         print("Hey! You didn't input an integer! You should input an integer now.")
-        numberinput=input()
-        collatz(numberinput)
+        numberinputabc=input()
+        collatz(numberinputabc)
 
+numberinput=input()
 collatz(numberinput)
